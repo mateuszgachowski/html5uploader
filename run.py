baa19c683f6217d68af86f613ece641608c7f0ad
Mateusz <mateusz.gachowski@gmail.com>
2012-08-26 14:33:09 +0300

    Update run.py
    Fix in file serving method, some comments added.

--- run.py
+++ run.py
@@ -16,28 +16,28 @@ def index():
 def uploader():
     if request.method == 'POST':
 	filename = None
-	if request.files and request.files.getlist('\'upload\''):
+	# files sent as FILES list
+	if request.files.getlist('\'upload\''):
 	    file = request.files.getlist('\'upload\'')[0]
-	    
-	    # check here file extenstion
 	    if file:
 		#remove ' and ' from filename
 		filename = file.filename.replace('\'', '')
 		file.save(os.path.join(save_path, filename))
 	    else:
 		return jsonify({'success': False, 'error': 'Extension not suported'})
-	    
+		
+	# support send image in headers
 	if request.args.get('up') and not request.files:
-	    if 'base64' in request.args and request.args['base64'] == 'true':
+	    if request.args.get('base64') and request.args['base64'] == 'true':
 		content = base64.b64decode(request.data) # b64
 	    else:
 		content = request.data
 
+	    # fix bug in some browsers - headers must be uppercase
 	    headers = {k.upper(): v for k, v in request.headers}
 		
 	    filename = headers.get('UP-FILENAME')
 	    path = os.path.join(save_path, filename)
-	    
 	    if filename:
 		f = open(path, 'w')
 		f.write(content)
@@ -46,12 +46,10 @@ def uploader():
 		return jsonify({'success': False, 'error': 'Image file not supported'})
 	return jsonify({'success': True, 'path': url_for('uploads', filename=filename), 'filename': filename})
 	
+# files serving method
 @app.route('/uploads/<filename>')
 def uploads(filename):
-    if filename:
 	return send_from_directory(save_path, filename)
-    else:
-	return None
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
